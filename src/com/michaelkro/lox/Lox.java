547b23f86de16664e366dbf3a9e6482549f1f138
Mike Ro <mikerodev@gmail.com>
2022-01-30 21:14:50 -0800

    initial commit

---
+++ src/com/michaelkro/lox/Lox.java
@@ -0,0 +1,42 @@
+package com.michaelkro.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      // To exit the REPL, the user can hit Control-D. This will signal an
+      // "end-of-file" condition, which causes readLine() to return null.
+      if (line == null) break;
+      run(line);
+    }
+  }
+
+  private static void run(String source) {}
+}
