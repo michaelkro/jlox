a969d317a344e07ceba127f8c3b12772b218ba56
Mike Ro <mikerodev@gmail.com>
2022-03-20 21:21:42 -0700

    Scanning: Rudimentary error handling

--- src/com/michaelkro/lox/Lox.java
+++ src/com/michaelkro/lox/Lox.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  static boolean hadError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -22,6 +23,8 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+
+    if (hadError) System.exit(65);
   }
 
   private static void runPrompt() throws IOException {
@@ -35,8 +38,21 @@ public class Lox {
       // "end-of-file" condition, which causes readLine() to return null.
       if (line == null) break;
       run(line);
+      hadError = false;
     }
   }
 
   private static void run(String source) {}
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println(
+      "[line " + line + "] Error" + where + ": " + message
+    );
+
+    hadError = true;
+  }
 }
