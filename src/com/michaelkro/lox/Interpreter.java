6a8f2cf555d6930c6099fb7d8eee7464eb8cb768
Mike Ro <mikerodev@gmail.com>
2022-04-24 20:49:10 -0700

    Evaluating Expressions: Literals and Groupings

---
+++ src/com/michaelkro/lox/Interpreter.java
@@ -0,0 +1,17 @@
+package com.michaelkro.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+  private Object evaluate(Expr expr) {
+    return expr.accept(this);
+  }
+
+  @Override
+  public Object visitGroupingExpr(Expr.Grouping expr) {
+    return evaluate(expr.expression);
+  }
+
+  @Override
+  public Object visitLiteralExpr(Expr.Literal expr) {
+    return expr.value;
+  }
+}
