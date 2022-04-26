6deef135cc8eb6916f1760d2e8324564545f89d3
Mike Ro <mikerodev@gmail.com>
2022-04-25 20:13:45 -0700

    Evaluating Expressions: Unary expressions

--- src/com/michaelkro/lox/Interpreter.java
+++ src/com/michaelkro/lox/Interpreter.java
@@ -14,4 +14,25 @@ class Interpreter implements Expr.Visitor<Object> {
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
   }
+
+  @Override
+  public Object visitUnaryExpr(Expr.Unary expr) {
+    Object right = expr.right;
+
+    switch (expr.operator.type) {
+      case BANG:
+        return !isTruthy(right);
+      case MINUS:
+        return -(double)right;
+    }
+
+    // Unreachable.
+    return null;
+  }
+
+  private boolean isTruthy(Object object) {
+    if (object == null) return false;
+    if (object instanceof Boolean) return (boolean)object;
+    return true;
+  }
 }
