735fa340af23c834b9e113c2ff5d3b3c151de16d
Mike Ro <mikerodev@gmail.com>
2022-04-30 20:09:30 -0700

    Evaluating Expressions: Hook up interpreter

--- src/com/michaelkro/lox/Interpreter.java
+++ src/com/michaelkro/lox/Interpreter.java
@@ -1,6 +1,14 @@
 package com.michaelkro.lox;
 
 class Interpreter implements Expr.Visitor<Object> {
+  void interpret(Expr expression) {
+    try {
+      Object value = evaluate(expression);
+      System.out.println(stringify(value));
+    } catch (RuntimeError error) {
+      Lox.runtimeError(error);
+    }
+  }
   private Object evaluate(Expr expr) {
     return expr.accept(this);
   }
@@ -99,5 +107,18 @@ class Interpreter implements Expr.Visitor<Object> {
     if (a == null) return false;
 
     return a.equals(b);
+  }
+
+  private String stringify(Object object) {
+    if (object == null) return "nil";
+    if (object instanceof Double) {
+      String text = object.toString();
+      if (text.endsWith(".0")) {
+        text = text.substring(0, text.length() - 2);
+      }
+      return text;
+    }
+
+    return object.toString();
   }
 }
